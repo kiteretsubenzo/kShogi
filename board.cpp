50830f7e28ed96024aa5ef4f7792b86fe0edc38a
Keiichiro Arahari <k16_arahari@yahoo.co.jp>
2018-04-19 02:27:38 +0900

    着手リストを作成するときに打ちの着手に設定ミスがあったのを修正

--- board.cpp
+++ board.cpp
@@ -484,6 +484,12 @@ bool Board::AddMove(PAWN roll, uchar fromx, uchar fromy, char tox, char toy, boo
 	PAWN capture = matrix[utoy][utox].pawn;
 
 	PAWN_MOVE move( roll, fromx, fromy, utox, utoy, matrix[fromy][fromx].pawn, matrix[utoy][utox].pawn, upgrade, 0 );
+	if (roll != PAWN_NONE)
+	{
+		move.from.x = 0;
+		move.from.y = 0;
+		move.from.pawn = PAWN_NONE;
+	}
 
 	Move(move);
 	// 負ける手は指さない
